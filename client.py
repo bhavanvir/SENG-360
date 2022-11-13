7bb1bd64ceef4d526d4c233cdfe8bba916661c07
Jas Dhaul <77562035+jd1771@users.noreply.github.com>
2022-11-13 11:34:05 -0800

    fixed small variable name issue.

--- client.py
+++ client.py
@@ -11,23 +11,23 @@ host = '127.0.0.1'
 port = 7000
 
 # connect to server
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect((host, port))
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.connect((host, port))
 
 def receive():
     while True:
         # receive message from server
         # if 'USER' send username, if 'PASS' send password, else print message
-        message = client.recv(1024).decode('ascii')
+        message = server.recv(1024).decode('ascii')
         if message == 'ACTION':
-            client.send('LOGIN'.encode('ascii'))
+            server.send('LOGIN'.encode('ascii'))
         elif message == 'USER':
-            client.send(username.encode('ascii'))
+            server.send(username.encode('ascii'))
         elif message == 'PASS':
-            client.send(password.encode('ascii'))
+            server.send(password.encode('ascii'))
         elif message == 'FAIL':
-            client.shutdown(socket.SHUT_RDWR)
-            client.close()
+            server.shutdown(socket.SHUT_RDWR)
+            server.close()
             # dirty solution to force program to exit without waiting for threads to finish
             os._exit(1)
         else:
@@ -37,10 +37,10 @@ def write():
     while True:
         # send message to server
         message = f'{input("")}'
-        client.send(message.encode('ascii'))
+        server.send(message.encode('ascii'))
 
         # send username to server
-        client.send(username.encode('ascii'))
+        server.send(username.encode('ascii'))
 
 def main():
     global username
@@ -49,11 +49,11 @@ def main():
     query = input("Do you want to login (1), register (2) or delete an account (3) ?: ")
     
     if query == "1" or query == "login":
-        client.send('LOGIN'.encode('ascii'))
+        server.send('LOGIN'.encode('ascii'))
     elif query == "2" or query == "register":
-        client.send('REGISTER'.encode('ascii'))
+        server.send('REGISTER'.encode('ascii'))
     elif query == "3" or query == "delete":
-        client.send('DELETE'.encode('ascii'))
+        server.send('DELETE'.encode('ascii'))
     else:
         print("Invalid input")
         main()
