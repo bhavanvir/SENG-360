87a7c752fbea64c2611dc51bf7a1b2a7522edf11
Jas Dhaul <77562035+jd1771@users.noreply.github.com>
2022-11-26 11:35:13 -0800

    Fixed a bug where only one client could connect to the server at a time

--- server.py
+++ server.py
@@ -23,127 +23,99 @@ def broadcast(message):
 
 # handle messages from clients
 def handle(client):
-    while True:
-        try:
-            # broadcast message
-            message = client.recv(1024).decode('ascii')
-            username = client.recv(1024).decode('ascii')
-            recipients = [user for user in usernames if user != username]
-            database.insert_message(message, recipients, username)
-            broadcast_message = f"{username}: {message}".encode('ascii')
-            broadcast(broadcast_message)
-        except:
-            # remove and close client connection
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            username = usernames[index]
-            broadcast(f'{username} has left the chat'.encode('ascii'))
-            usernames.remove(username)
-            break
+   
+    # Variable to check if successful user authentication has occured
+    SUCCESS_LOGIN = False
+    
+    action = client.recv(1024).decode('ascii')
+    # request and store username and password
+    client.send('USER'.encode('ascii'))
+    username = client.recv(1024).decode('ascii')
+    usernames.append(username)
+    client.send('PASS'.encode('ascii'))
+    password = client.recv(1024).decode('ascii')
+    
+    if action == "LOGIN":
+        if database.check_password(username, password):
+            print(f"Succeeded in logging in client with username {username}")
+            client.send(f"Successfully logged in as {username}".encode('ascii'))
+            SUCCESS_LOGIN = True
+        else:
+            print(f"Failed in logging in client with username {username}")
+            client.send(f"Failed to log in as {username}".encode('ascii'))
+            client.send('FAIL'.encode('ascii'))
+    elif action == "REGISTER":
+        if database.insert_user(username, password):
+            print(f"Registered client with username {username}")
+            client.send(f"Successfully registered as {username}".encode('ascii'))
+            SUCCESS_LOGIN = True
+        else:
+            client.send(f"{username} already exists".encode('ascii'))
+            client.send('FAIL'.encode('ascii'))
+            print(f"Attemped to register client with username {username}, but it already exists")
+    elif action == "DELETE":
+        if database.check_password(username, password):
+            print(f"Deleting user {username}")
+            deleted = database.delete_user(username)
+            if deleted:
+                print(f"Sucessfully deleted user {username}")
+                client.send(f"Sucessfully deleted user {username}".encode('ascii'))
+                client.send('FAIL'.encode('ascii'))
+            else:
+                print(f"Error deleting user {username}")
+                client.send(f"Error deleting user {username}".encode('ascii'))
+                client.send('FAIL'.encode('ascii'))
+        else:
+            print(f"Failed to find user {username}")
+            client.send(f"Invalid user credentials for {username}".encode('ascii'))
+            client.send('FAIL'.encode('ascii'))
+    elif action == "HISTORY":
+        if database.message_history(username):
+            print(f"Succesfully deleted message history for {username}")
+            history = database.message_history(username)
+            database.delete_messages(username)
+            client.send(history.encode('ascii'))
+        else:
+            print(f"Failed to delete message history for {username}")
+            client.send(f"Failed to find message history for {username}".encode('ascii'))
+            client.send('FAIL'.encode('ascii'))
+    if SUCCESS_LOGIN == True:
+        # Display successful authentication message
+        clients.append(client)
+        # Show the messaging options to the user
+        client.send('SHOW_MESSAGING_OPTIONS'.encode('ascii'))
+        # Now that the user has logged in, we will process the subsequent messaging requests
+        while True:
+            data = client.recv(1024)
+            data_obj = pickle.loads(data)
+            action = data_obj[0]
+            if action == "SEND_MSG":
+                recipient, message = data_obj[1], data_obj[2]
+                if database.user_exists(recipient):
+                    database.insert_message(message, recipient, username)
+                    print(f"Sent message to: {recipient}")
+                    client.send(f"Sent messsage to {recipient}".encode('ascii'))
+                else:
+                    print(f"Username: {username} does not exist, therefore the message could not be sent")
+                    client.send(f"Username: {username} does not exist, therefore the message could not be sent".encode('ascii'))
+            elif action == "GET_HISTORY":
+                recipient = data_obj[1]
+                # Send message packets to the client
+                messages = database.get_message_history_between_users(username, recipient)
+                package = pickle.dumps(messages)
+                client.send(package)
 
 # receive and broadcast messages
 def receive():
     while True:
         
-        # Variable to check if successful user authentication has occured
-        SUCCESS_LOGIN = False
-
         # accept connection
         client, address = server.accept()
         print(f"Connected with {str(address)}")
-
-        action = client.recv(1024).decode('ascii')
-
-        # request and store username and password
-        client.send('USER'.encode('ascii'))
-        username = client.recv(1024).decode('ascii')
-        usernames.append(username)
-
-        client.send('PASS'.encode('ascii'))
-        password = client.recv(1024).decode('ascii')
-
-        if action == "LOGIN":
-            if database.check_password(username, password):
-                print(f"Succeeded in logging in client with username {username}")
-                client.send(f"Successfully logged in as {username}".encode('ascii'))
-                SUCCESS_LOGIN = True
-            else:
-                print(f"Failed in logging in client with username {username}")
-                client.send(f"Failed to log in as {username}".encode('ascii'))
-                client.send('FAIL'.encode('ascii'))
-        elif action == "REGISTER":
-            if database.insert_user(username, password):
-                print(f"Registered client with username {username}")
-                client.send(f"Successfully registered as {username}".encode('ascii'))
-                SUCCESS_LOGIN = True
-            else:
-                client.send(f"{username} already exists".encode('ascii'))
-                client.send('FAIL'.encode('ascii'))
-                print(f"Attemped to register client with username {username}, but it already exists")
-        elif action == "DELETE":
-            if database.check_password(username, password):
-                print(f"Deleting user {username}")
-                deleted = database.delete_user(username)
-                if deleted:
-                    print(f"Sucessfully deleted user {username}")
-                    client.send(f"Sucessfully deleted user {username}".encode('ascii'))
-                    client.send('FAIL'.encode('ascii'))
-                else:
-                    print(f"Error deleting user {username}")
-                    client.send(f"Error deleting user {username}".encode('ascii'))
-                    client.send('FAIL'.encode('ascii'))
-            else:
-                print(f"Failed to find user {username}")
-                client.send(f"Invalid user credentials for {username}".encode('ascii'))
-                client.send('FAIL'.encode('ascii'))
-        elif action == "HISTORY":
-            if database.message_history(username):
-                print(f"Succesfully deleted message history for {username}")
-                history = database.message_history(username)
-                database.delete_messages(username)
-                client.send(history.encode('ascii'))
-            else:
-                print(f"Failed to delete message history for {username}")
-                client.send(f"Failed to find message history for {username}".encode('ascii'))
-                client.send('FAIL'.encode('ascii'))
-
-        if SUCCESS_LOGIN == True:
-            # Display successful authentication message
-            clients.append(client)
-
-            # Show the messaging options to the user
-            client.send('SHOW_MESSAGING_OPTIONS'.encode('ascii'))
-
-            # Now that the user has logged in, we will process the subsequent messaging requests
-            while True:
-                data = client.recv(1024)
-                data_obj = pickle.loads(data)
-                action = data_obj[0]
-                if action == "SEND_MSG":
-                    recipient, message = data_obj[1], data_obj[2]
-                    if database.user_exists(recipient):
-                        database.insert_message(message, recipient, username)
-                        print(f"Sent message to: {recipient}")
-                        client.send(f"Sent messsage to {recipient}".encode('ascii'))
-                    else:
-                        print(f"Username: {username} does not exist, therefore the message could not be sent")
-                        client.send(f"Username: {username} does not exist, therefore the message could not be sent".encode('ascii'))
-                elif action == "GET_HISTORY":
-                    recipient = data_obj[1]
-                    # Send message packets to the client
-                    messages = database.get_message_history_between_users(username, recipient)
-                    package = pickle.dumps(messages)
-                    client.send(package)
-
-                    
-            # print and broadcast username
-            #broadcast(f"{username} joined the chat\n".encode('ascii'))
-            #client.send('Connected to the server'.encode('ascii'))
-
-            # start handling thread for client
-            thread = threading.Thread(target=handle, args=(client,))
-            thread.start()
+           
+         # start handling thread for client
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
 
 def main():
     print("Server is listening...")
